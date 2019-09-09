7ccc567fbceea36b1e4953898458a540036ffdb5
Samir Mejia <samir04m@gmail.com>
2019-09-08 19:39:48 -0500

    Sitio de administaracion para modelos

--- miscuentas/apps/contabilidad/admin.py
+++ miscuentas/apps/contabilidad/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Cuenta)
+admin.site.register(Etiqueta)
+admin.site.register(Persona)
+admin.site.register(Transaccion)
