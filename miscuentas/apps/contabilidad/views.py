343c808ed9d687c100613615085786bba1a8d52a
Samir Mejia <samir04m@gmail.com>
2019-09-08 22:14:57 -0500

    Antes de heroku

--- miscuentas/apps/contabilidad/views.py
+++ miscuentas/apps/contabilidad/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
 def home(request):
     return render(request, 'index.html')
