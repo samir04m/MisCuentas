ef0c366079663015a8db81b5152d18e8ec18cb23
Samir Mejia <samir04m@gmail.com>
2019-09-20 20:46:54 -0500

    Correccion en listar transacciones y creacion de vista ingresos diarios

--- miscuentas/apps/reporte/urls.py
+++ miscuentas/apps/reporte/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [
-    path('diario/', diario, name='diario'),
-    path('mensual/', mensual, name='mensual'),
+    path('diario-egreso/', diario_egreso, name='diario_egreso'),
+    path('diario-ingreso/', diario_ingreso, name='diario_ingreso'),
+    path('mensual-egreso/', mensual_egreso, name='mensual_egreso'),
 ]
--- miscuentas/apps/reporte/views.py
+++ miscuentas/apps/reporte/views.py
@@ -6,7 +6,7 @@ from django.db.models.functions import TruncMonth, TruncDay
 from django.db.models import Count, Sum
 
 @login_required
-def diario(request):
+def diario_egreso(request):
     egresos =  reversed( Transaccion.objects.filter(cuenta__user = request.user.id, tipo='egreso') \
             .annotate(day=TruncDay('fecha')).values('day').annotate(nt=Count('id'), total=Sum('cantidad')).order_by() )
 
@@ -15,11 +15,23 @@ def diario(request):
 
 
     context = {"egresos":egresos, "chart_egresos":chart_egresos}
-    return render(request, 'reporte/diario.html', context)
+    return render(request, 'reporte/diario_egreso.html', context)
+
+@login_required
+def diario_ingreso(request):
+    ingresos =  reversed( Transaccion.objects.filter(cuenta__user = request.user.id, tipo='ingreso') \
+            .annotate(day=TruncDay('fecha')).values('day').annotate(nt=Count('id'), total=Sum('cantidad')).order_by() )
+
+    chart_ingresos = Transaccion.objects.filter(cuenta__user = request.user.id, tipo='ingreso') \
+            .annotate(day=TruncDay('fecha')).values('day').annotate(nt=Count('id'), total=Sum('cantidad')).order_by()[:7]
+
+
+    context = {"ingresos":ingresos, "chart_ingresos":chart_ingresos}
+    return render(request, 'reporte/diario_ingreso.html', context)
 
 
 @login_required
-def mensual(request):
+def mensual_egreso(request):
     egresos = Transaccion.objects.filter(cuenta__user = request.user.id, tipo='egreso'). \
         annotate(month=TruncMonth('fecha')).values('month').annotate(nt=Count('id'), total=Sum('cantidad')).order_by()
     print(egresos)
