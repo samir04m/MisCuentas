ead9c85b663a91fbf422208b1977d6f58791eb45
Samir Mejia <samir04m.dev@gmail.com>
2022-11-21 17:07:54 -0500

    Se elimino carpetas de venv

--- dj2_postgres/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/darkblue/Documentos/djangoProjets/MisCuentas/dj2_postgres/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv_windows/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\S4M1R\Documents\_Workspace\django projects\MisCuentas\venv_windows\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
