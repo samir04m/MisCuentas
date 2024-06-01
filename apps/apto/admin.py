b68e5fd09567c49c47132eb3fdbe528a8fb058e9
Samir Mejia <samir04m.dev@gmail.com>
2024-06-01 00:51:14 -0500

    Creación de modelos y admin (apto)

--- apps/apto/admin.py
+++ apps/apto/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import *
 
-# Register your models here.
+class ApartamentoResource(resources.ModelResource):
+    class Meta:
+        model = Apartamento
+
+class ApartamentoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','nombre']
+    list_display = ('id','nombre', )
+    resource_class = ApartamentoResource
+
+class PagadorResource(resources.ModelResource):
+    class Meta:
+        model = Pagador
+
+class PagadorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','user__username']
+    list_display = ('id','user',)
+    resource_class = PagadorResource
+
+class PersonaResource(resources.ModelResource):
+    class Meta:
+        model = Persona
+
+class PersonaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','nombre','pagador__user__username']
+    list_display = ('id','nombre','pagador',)
+    resource_class = PersonaResource
+
+class EstadiaResource(resources.ModelResource):
+    class Meta:
+        model = Estadia
+
+class EstadiaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','fechaInicio','fechaFin']
+    list_display = ('id','fechaInicio','fechaFin','persona',)
+    resource_class = EstadiaResource
+
+class EmpresaResource(resources.ModelResource):
+    class Meta:
+        model = Empresa
+
+class EmpresaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','nombre']
+    list_display = ('id','nombre', )
+    resource_class = EmpresaResource
+
+class ReciboResource(resources.ModelResource):
+    class Meta:
+        model = Recibo
+
+class ReciboAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','periodo','valorPago','fechaInicio','fechaFin']
+    list_display = ('id','periodo','valorPago','fechaInicio','fechaFin','empresa',)
+    resource_class = ReciboResource
+
+class PagadorReciboResource(resources.ModelResource):
+    class Meta:
+        model = PagadorRecibo
+
+class PagadorReciboAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['id','valorPago','pagador','recibo']
+    list_display = ('id','valorPago','pagador','recibo',)
+    resource_class = PagadorReciboResource
+
+admin.site.register(Pagador, PagadorAdmin)
+admin.site.register(Apartamento, ApartamentoAdmin)
+admin.site.register(Persona, PersonaAdmin)
+admin.site.register(Estadia, EstadiaAdmin)
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Recibo, ReciboAdmin)
+admin.site.register(PagadorRecibo, PagadorReciboAdmin)
--- apps/apto/models.py
+++ apps/apto/models.py
@@ -1,2 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Apartamento(models.Model):
+    nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Pagador(models.Model):
+    apto = models.ForeignKey(Apartamento, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}".format(self.user.username)
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    pagador = models.ForeignKey(Pagador, null=False, blank=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Estadia(models.Model):
+    fechaInicio = models.CharField(max_length=100)
+    fechaFin = models.CharField(max_length=100)
+    persona = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} - {} | {}".format(self.fechaInicio, self.fechaFin, self.persona.nombre)
+
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Recibo(models.Model):
+    periodo = models.CharField(max_length=50)
+    valorPago = models.IntegerField()
+    fechaInicio = models.CharField(max_length=100)
+    fechaFin = models.CharField(max_length=100)
+    diasFacturados = models.IntegerField()
+    empresa = models.ForeignKey(Empresa, null=False, blank=False, on_delete=models.CASCADE)
+
+class PagadorRecibo(models.Model):
+    valorPago = models.IntegerField()
+    pagador = models.ForeignKey(Pagador, null=False, blank=False, on_delete=models.CASCADE)
+    recibo = models.ForeignKey(Recibo, null=False, blank=False, on_delete=models.CASCADE)
